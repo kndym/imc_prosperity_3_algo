2098bb41827130fc2f839a8b9e22c85c3bfc0f02
kndym <kevin.nday12@gmail.com>
2025-03-26 18:18:57 -0400

    edited volume

--- example.py
+++ example.py
@@ -2,12 +2,16 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 
+class TradeHistory:
+    def __init__(self, length=10):
+        self.ask_list=[]
+
+
 class Trader:
-    
     def run(self, state: TradingState):
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
-        trade_dict={"RAINFOREST_RESIN": [9995, 10000], 
+        trade_dict={"RAINFOREST_RESIN": [10000, 10000], 
                     "KELP": [2015, 2020]}
         result = {}
         for product in state.order_depths:
@@ -17,16 +21,16 @@ class Trader:
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
             buy_low, sell_high = trade_dict[product]
             if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < buy_low:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
+                for ask, ask_amount in order_depth.sell_orders.items():
+                    if int(ask) < buy_low:
+                        print("BUY", str(-ask_amount) + "x", ask)
+                        orders.append(Order(product, ask, -ask_amount))
     
             if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > sell_high:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                 for bid, bid_amount in order_depth.sell_orders.items():
+                    if int(ask) > sell_high:
+                        print("BUY", str(-bid_amount) + "x", bid)
+                        orders.append(Order(product, bid, -bid_amount))
             
             result[product] = orders
     
@@ -37,3 +41,4 @@ class Trader:
 				# Sample conversion request. Check more details below. 
         conversions = 1
         return result, conversions, traderData
+    
