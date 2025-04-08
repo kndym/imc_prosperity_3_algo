59d0498918237a0047053d7aa1625b8c6431f9df
kndym <kevin.nday12@gmail.com>
2025-04-08 13:56:30 -0400

    work 4/8

--- round_1_v2.py
+++ round_1_v2.py
@@ -1,26 +1,20 @@
 from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
 from typing import List
-import string
 import numpy as np
-import scipy.stats as st
-import json
+import statistics as stat
 from typing import List, Dict, Any
-from logger import Logger
-
 
 class Trader:
     """Main trading class implementing different strategies for different products."""
     
     PRODUCT_PARAMS = {
-        "RAINFOREST_RESIN": {"fp_mean":10000, "fp_dev":1.5, "limit":50},
-        "KELP": {"buy_low": 2015, "sell_high": 2020},
-        "AMETHYSTS": {"fp_mean": 10000, "fp_dev":0.01, "limit":50},
-        "STARFRUIT": {"fp_mean":5000, "fp_dev":5, "limit":50, "sigma":1}
+        "RAINFOREST_RESIN": {"fp":10000, "limit":50},
+        "KELP": {"limit":50},
+        "SQUID_INK": {"limit":50}
     }
     
     def __init__(self):
-        self.logger = Logger()
-        self.active_products = ["RAINFOREST_RESIN", "KELP"]  
+        self.active_products = ["RAINFOREST_RESIN", "KELP", "SQUID_INK"]  
 
     def prob_algo(self, product: str, 
                   final_price_mean: float, final_price_dev: float,
@@ -31,8 +25,8 @@ class Trader:
                          move_down_prices: bool) -> list:
             def gaussian_cdf(price: int, final_price_mean: float, 
                              final_price_dev: float, normal_cdf: bool) -> float:
-                normal_z=(price-final_price_mean)/final_price_dev
-                prob=st.norm.cdf(normal_z)
+                dist=stat.NormalDist(mu=final_price_mean, sigma=final_price_dev)
+                prob=dist.cdf(price)
                 if normal_cdf:
                     return prob
                 else:
@@ -42,7 +36,8 @@ class Trader:
             for price, volume in sorted(order_dict.items(), key=lambda item: item[0], reverse=move_down_prices):
                 real_volume=abs(volume)
                 unscaled_volume=gaussian_cdf(price, final_price_mean, final_price_dev, move_down_prices)
-                current_max_volume=int(unscaled_volume*max_volume//1)
+                current_max_volume=unscaled_volume*1000
+                #int(unscaled_volume*max_volume//1)
                 if current_max_volume>current_volume:
                     this_price_max=current_max_volume-current_volume
                     trade_volume=min(this_price_max, real_volume)
@@ -70,15 +65,34 @@ class Trader:
         for price, real_volume in possible_sells:
             orders.append(Order(product, price, -real_volume))
 
+
+    def stable_algo(self, product: str, 
+                  fair_price: float,
+                  order_depth: OrderDepth, orders: List[Order]) -> None:
+        if len(order_depth.sell_orders)!=0:
+            for ask, ask_amount in order_depth.sell_orders.items():
+                if int(ask)<fair_price:
+                    orders.append(Order(product, ask, -ask_amount))  
+        if len(order_depth.buy_orders)!=0:
+            for bid, bid_amount in order_depth.buy_orders.items():
+                if int(bid)>fair_price:
+                    orders.append(Order(product, bid, -bid_amount)) 
+    
+    def volitile_algo(self, product: str, 
+                  order_depth: OrderDepth, orders: List[Order]) -> None:
+        if len(order_depth.sell_orders.keys())>1:
+            ask, ask_amount = list(order_depth.sell_orders.items())[0]
+            orders.append(Order(product, ask, -ask_amount)) 
+        if len(order_depth.buy_orders.keys())>1:
+            bid, bid_amount = list(order_depth.buy_orders.items())[0]
+            orders.append(Order(product, bid, -bid_amount)) 
+
+
     def current_mid_price(self, order_depth: OrderDepth) -> float:
         return (list(order_depth.buy_orders.items())[0][0]
                 +list(order_depth.sell_orders.items())[0][0])/2
 
     def run(self, state: TradingState) -> tuple[Dict[str, List[Order]], int, str]:
-        """Main trading method called each timestamp."""
-        self.logger.print("traderData:", state.traderData)
-        self.logger.print("Observations:", str(state.observations))
-        time=state.timestamp/100
         result = {}
         
         for symbol, order_depth in state.order_depths.items():
@@ -88,19 +102,11 @@ class Trader:
             except KeyError:
                 position=0
             params = self.PRODUCT_PARAMS.get(symbol, {})
-            if symbol == self.active_products[1]:  # STARFRUIT/KELP
-                current_price=self.current_mid_price(order_depth)
-                current_var=params["sigma"]*(10000.1-time)
-                param_price=params["fp_mean"]
-                param_var=params["fp_dev"]**2
-                final_var=1/(1/current_var+1/param_var)
-                final_price=(current_price/current_var+param_price/param_var)*final_var
-                self.prob_algo(symbol, final_price, np.sqrt(final_var),
-                               order_depth, orders, position, params["limit"]
-                               )
-            elif symbol == self.active_products[0]:  # AMETHYSTS/RESIN
+            if symbol == self.active_products[0]:  # STARFRUIT/KELP
+                fair=params["fp"]
+                self.stable_algo(symbol, fair, order_depth, orders)
+            else:
                 pass
             result[symbol] = orders
-        self.logger.flush(state, result, 1, "SAMPLE")
         return result, 1, "SAMPLE"
     
